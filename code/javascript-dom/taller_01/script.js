0e0be0a05834545f4b810c95e0574a2cdc521b54
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-06-06 19:22:14 -0500

    Javascript DOM

---
+++ code/javascript-dom/taller_01/script.js
@@ -0,0 +1,9 @@
+function cargar_elemento() {
+    var container = document.getElementById('container')
+
+    var h1 = document.createElement('h1')
+    var texto = document.createTextNode('Programacion y Plataformas Web')
+    h1.appendChild(texto)
+    
+    container.appendChild(h1)
+}
