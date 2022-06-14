75dfaed05b34bb1f10766d4ee1a939ed8b660540
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-06-13 19:16:45 -0500

    Taller 1, tipos de datos.

---
+++ code/javascript-basico/taller_01/script.js
@@ -0,0 +1,34 @@
+// Number
+var a = 1
+console.log( typeof a )
+var b = 1.5
+console.log( typeof b )
+// String
+var c = 'Hola mundo'
+console.log( typeof c )
+// Boolean
+var d = true
+console.log( typeof d )
+
+// null
+var e = null
+console.log( e )
+console.log( typeof e )
+// undefined
+var f
+console.log( f )
+console.log( typeof f )
+
+// Objetos
+var persona = {
+    nombre: 'Angie',
+    apellido: 'Gomez',
+    correo_electronico: 'agomez@est.ups.edu.ec'
+}
+console.log( persona )
+console.log( typeof persona )
+console.log( persona.nombre )
+
+persona.nombre = 'Angie Lissette'
+console.log( persona )
+console.log( persona.nombre.length )
