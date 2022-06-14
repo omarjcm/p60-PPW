9dbdb7fec458a46eeb8d27cb72c7aeb14d03ee8c
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-06-13 19:32:55 -0500

    Comparaciones.

---
+++ code/javascript-basico/taller_02/script.js
@@ -0,0 +1,34 @@
+// Se compara el valor.
+console.log(1 == 1)
+console.log(1 == '1')
+console.log(21 == '21')
+
+//console.log(1 = 1) esto es un error
+
+// Se compara el valor y el tipo de dato.
+console.log(1 === 1)
+console.log(1 === '1')
+console.log(21 === '21')
+console.log('21' === '21')
+
+console.log(1 == true)
+console.log(0 == false)
+
+console.log(1 === true)
+
+console.log( 0 == null )
+console.log( false == null )
+
+console.log( 0 == undefined )
+
+console.log( null == undefined)
+console.log( null === undefined)
+
+console.log( null )
+console.log( !null )
+console.log( !!null )
+console.log( 0 == !!null )
+
+console.log( undefined )
+console.log( !undefined )
+console.log( !!undefined )
