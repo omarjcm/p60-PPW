24cc45866457790c0f58da358891b171f4c2f5bc
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-06-15 21:13:57 -0500

    Fomulario.

--- code/javascript-basico/taller_07/script.js
+++ code/javascript-basico/taller_07/script.js
@@ -1,1 +1,15 @@
-const guardar = function() 
+var contactos = []
+
+const guardar = function() {
+    let nombre_ = document.getElementById('nombre').value
+    let apellido_ = document.getElementById('apellido').value
+    let correo_ = document.getElementById('correo').value
+
+    let objeto = {
+        nombre: nombre_,
+        apellido: apellido_,
+        correo: correo_
+    }
+
+    contactos.push(objeto)
+}
