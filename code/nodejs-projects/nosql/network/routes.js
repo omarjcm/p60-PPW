bd4c17e72edfe339f09f4839d4de73569597ad99
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-08-22 19:17:53 -0500

    TALLER - CRUD de File

--- code/nodejs-projects/nosql/network/routes.js
+++ code/nodejs-projects/nosql/network/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const user = require('../components/user/network')
+const file = require('../components/file/network')
 
 const routes = function(server) {
     server.use('/user', user)
+    server.use('/file', file)
 }
 
 module.exports = routes
